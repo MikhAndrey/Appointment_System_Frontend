4dd8780243832b7fb20e4346f04d6e242742b9ba
MikhAndrey <mikhalevange@gmail.com>
2024-03-24 14:32:08 +0300

    Fixed typescript support issues, made components class-based

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import "../src/assets/main.css"
 import router from "./router";
 import Buefy from '@ntohq/buefy-next'
-import 'buefy/dist/buefy.css';
+import '@ntohq/buefy-next/dist/buefy.css';
 import {store} from "./models/store";
 
+// @ts-ignore
 createApp(App).use(router).use(Buefy).use(store).mount("#app");
--- src/models/store.ts
+++ src/models/store.ts
@@ -1,27 +1,32 @@
 import {createStore} from "vuex";
 import {UserInfo} from "./account.model";
 
+interface storeState {
+    userInfo: UserInfo | undefined,
+    pageSize: number
+}
+
 export const store = createStore({
     state: {
         userInfo: undefined as UserInfo | undefined,
         pageSize: 10
     },
     mutations: {
-        setUserInfo (state, userInfo: UserInfo | undefined) {
+        setUserInfo (state: storeState, userInfo: UserInfo | undefined) {
             state.userInfo = userInfo;
         },
-        setPageSize (state, pageSize: number) {
+        setPageSize (state: storeState, pageSize: number) {
             state.pageSize = pageSize;
         }
     },
     getters: {
-        getUserInfo (state): UserInfo | undefined {
+        getUserInfo (state: storeState): UserInfo | undefined {
             return state.userInfo;
         },
-        getPageSize (state) {
+        getPageSize (state: storeState) {
             return state.pageSize;
         },
-        isInPermission(state, permissionName: string): 
+        isInPermission(state: storeState, permissionName: string): 
             (permission: string) => boolean | undefined {
             return permissionName => state.userInfo?.permissions.some(p => p === permissionName);
         }
